f8f56fb4733f9514f3cc18aebfda65d07411d4bf
j2softj2 <j2softj2@gmail.com>
2020-02-12 16:45:09 +0100

    UI Calculadora 12-2-20

--- pokestics/src/pokestics/Principal.java
+++ pokestics/src/pokestics/Principal.java
@@ -21,10 +21,30 @@ import java.awt.Dialog.ModalityType;
 import java.awt.event.KeyEvent;
 import javax.swing.KeyStroke;
 import java.awt.event.InputEvent;
+import javax.swing.JSeparator;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Insets;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
 
 public class Principal extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField campoApuesta;
+	private JTextField campoBote;
+	private JTextField campoProbMano;
+	private JTextField campoProbCarta;
 
 	/**
 	 * Launch the application.
@@ -43,15 +63,165 @@ public class Principal extends JDialog {
 	 * Create the dialog.
 	 */
 	public Principal() {
+		setResizable(false);
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setMinimumSize(new Dimension(500, 500));
 		setBounds(100, 100, 1069, 685);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBackground(new Color(60, 179, 113));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JSeparator separadorCalculadora = new JSeparator();
+		separadorCalculadora.setBounds(751, 5, 2, 619);
+		separadorCalculadora.setBackground(new Color(255, 255, 255));
+		separadorCalculadora.setOrientation(SwingConstants.VERTICAL);
+		contentPanel.add(separadorCalculadora);
+		
+		JLabel etCalculadora = new JLabel("Calculadora");
+		etCalculadora.setForeground(new Color(255, 255, 255));
+		etCalculadora.setBounds(855, 5, 134, 24);
+		etCalculadora.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 20));
+		contentPanel.add(etCalculadora);
+		
+		JLabel etCartasPropias = new JLabel("Cartas propias");
+		etCartasPropias.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 12));
+		etCartasPropias.setForeground(new Color(255, 255, 255));
+		etCartasPropias.setBounds(763, 47, 101, 24);
+		contentPanel.add(etCartasPropias);
+		
+		JSpinner spinnerCartaPropia1 = new JSpinner();
+		spinnerCartaPropia1.setToolTipText("Introduzca la primera carta de su mano : C-Corazones P-Picas T-Treboles D-Diamantes");
+		spinnerCartaPropia1.setModel(new SpinnerListModel(new String[] {"AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T", "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P", "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"}));
+		spinnerCartaPropia1.setBounds(892, 50, 44, 20);
+		contentPanel.add(spinnerCartaPropia1);
+		
+		JSpinner spinnerCartaPropia2 = new JSpinner();
+		spinnerCartaPropia2.setToolTipText("Introduzca la segunda carta de su mano : C-Corazones P-Picas T-Treboles D-Diamantes");
+		spinnerCartaPropia2.setModel(new SpinnerListModel(new String[] {"AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T", "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P", "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"}));
+		spinnerCartaPropia2.setBounds(956, 50, 44, 20);
+		contentPanel.add(spinnerCartaPropia2);
+		
+		JLabel etPosicionMesa = new JLabel("Posici\u00F3n en mesa");
+		etPosicionMesa.setForeground(Color.WHITE);
+		etPosicionMesa.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 12));
+		etPosicionMesa.setBounds(763, 82, 117, 24);
+		contentPanel.add(etPosicionMesa);
+		
+		JList list = new JList();
+		list.setBorder(new TitledBorder(new LineBorder(new Color(0, 128, 0)), "Posici\u00F3n", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 128, 0)));
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"DEALER", "SB", "BB", "3", "4", "5", "6", "7", "8", "9"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list.setBounds(779, 111, 85, 206);
+		contentPanel.add(list);
+		
+		JLabel etCartasComunitarias = new JLabel("Cartas comunitarias");
+		etCartasComunitarias.setForeground(Color.WHITE);
+		etCartasComunitarias.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 12));
+		etCartasComunitarias.setBounds(902, 81, 134, 24);
+		contentPanel.add(etCartasComunitarias);
+		
+		JSpinner spinnerCarta1 = new JSpinner();
+		spinnerCarta1.setModel(new SpinnerListModel(new String[] {"AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T", "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P", "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"}));
+		spinnerCarta1.setToolTipText("Carta comunitaria 1 (Flop)");
+		spinnerCarta1.setBounds(945, 111, 44, 20);
+		contentPanel.add(spinnerCarta1);
+		
+		JSpinner spinnerCarta2 = new JSpinner();
+		spinnerCarta2.setModel(new SpinnerListModel(new String[] {"AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T", "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P", "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"}));
+		spinnerCarta2.setToolTipText("Carta comunitaria 2 (Flop)");
+		spinnerCarta2.setBounds(945, 142, 44, 20);
+		contentPanel.add(spinnerCarta2);
+		
+		JSpinner spinnerCarta3 = new JSpinner();
+		spinnerCarta3.setModel(new SpinnerListModel(new String[] {"AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T", "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P", "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"}));
+		spinnerCarta3.setToolTipText("Carta comunitaria 3 (Flop)");
+		spinnerCarta3.setBounds(945, 173, 44, 20);
+		contentPanel.add(spinnerCarta3);
+		
+		JSpinner spinnerCarta4 = new JSpinner();
+		spinnerCarta4.setModel(new SpinnerListModel(new String[] {"AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T", "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P", "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"}));
+		spinnerCarta4.setToolTipText("Carta comunitaria 4 (Turn)");
+		spinnerCarta4.setBounds(945, 204, 44, 20);
+		contentPanel.add(spinnerCarta4);
+		
+		JSpinner spinnerCarta5 = new JSpinner();
+		spinnerCarta5.setModel(new SpinnerListModel(new String[] {"AC", "KC", "QC", "JC", "10C", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AT", "KT", "QT", "JT", "10T", "9T", "8T", "7T", "6T", "5T", "4T", "3T", "2T", "AP", "KP", "QP", "JP", "10P", "9P", "8P", "7P", "6P", "5P", "4P", "3P", "2P", "AD", "KD", "QD", "JD", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"}));
+		spinnerCarta5.setToolTipText("Carta comunitaria 5 (Rivern)");
+		spinnerCarta5.setBounds(945, 235, 44, 20);
+		contentPanel.add(spinnerCarta5);
+		
+		JLabel etApuesta = new JLabel("Apuesta");
+		etApuesta.setForeground(Color.WHITE);
+		etApuesta.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 12));
+		etApuesta.setBounds(769, 349, 60, 24);
+		contentPanel.add(etApuesta);
+		
+		campoApuesta = new JTextField();
+		campoApuesta.setToolTipText("Introduzca la cantidad a apostar");
+		campoApuesta.setBounds(839, 352, 60, 20);
+		contentPanel.add(campoApuesta);
+		campoApuesta.setColumns(10);
+		
+		JLabel etBote = new JLabel("Bote");
+		etBote.setForeground(Color.WHITE);
+		etBote.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 12));
+		etBote.setBounds(926, 349, 44, 24);
+		contentPanel.add(etBote);
+		
+		campoBote = new JTextField();
+		campoBote.setToolTipText("Introduzca el bote de la mesa");
+		campoBote.setColumns(10);
+		campoBote.setBounds(976, 352, 60, 20);
+		contentPanel.add(campoBote);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(773, 383, 263, 2);
+		contentPanel.add(separator);
+		
+		JLabel etResultado = new JLabel("Resultado");
+		etResultado.setForeground(Color.WHITE);
+		etResultado.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 20));
+		etResultado.setBounds(855, 396, 117, 24);
+		contentPanel.add(etResultado);
+		
+		JLabel etProbabilidadMano = new JLabel("Probabilidad de mano");
+		etProbabilidadMano.setForeground(Color.WHITE);
+		etProbabilidadMano.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 12));
+		etProbabilidadMano.setBounds(833, 431, 144, 24);
+		contentPanel.add(etProbabilidadMano);
+		
+		JLabel etProbabilidadCarta = new JLabel("Probabilidad de carta necesaria");
+		etProbabilidadCarta.setForeground(Color.WHITE);
+		etProbabilidadCarta.setFont(new Font("DejaVu Serif Condensed", Font.BOLD, 12));
+		etProbabilidadCarta.setBounds(822, 508, 197, 24);
+		contentPanel.add(etProbabilidadCarta);
+		
+		campoProbMano = new JTextField();
+		campoProbMano.setBounds(779, 466, 257, 31);
+		contentPanel.add(campoProbMano);
+		campoProbMano.setColumns(10);
+		
+		campoProbCarta = new JTextField();
+		campoProbCarta.setColumns(10);
+		campoProbCarta.setBounds(779, 543, 257, 31);
+		contentPanel.add(campoProbCarta);
+		
+		JButton botonCalcular = new JButton("CALCULAR");
+		botonCalcular.setBounds(872, 585, 89, 23);
+		contentPanel.add(botonCalcular);
 		
 		JMenuBar barraMenu = new JMenuBar();
+		barraMenu.setBackground(new Color(255, 255, 255));
 		setJMenuBar(barraMenu);
 		
 		JMenu menuArchivo = new JMenu("Archivo");
@@ -105,5 +275,4 @@ public class Principal extends JDialog {
 	private void cerrarAplicacion() {
 		this.dispose();
 	}
-
 }
